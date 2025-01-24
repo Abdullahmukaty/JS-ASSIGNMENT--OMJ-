e0820ffb56290ce4a3f979003b105b0e96f4051c
Abdullahmukaty <abdullahmukaty68@gmail.com>
2025-01-24 23:32:12 +0500

    updated

--- Assignment 1/index.js
+++ Assignment 1/index.js
@@ -3,12 +3,16 @@ alert("This Is Grade Calculator App")
 let studentname = prompt("Enter Your Name");
 console.log(studentname);
 
+let grandtotalmarks = 400
+console.log(grandtotalmarks)
+
 let subject1 = parseInt(prompt("Marks in English"))
 if (subject1 > 0 && subject1 < 101) {
     console.log("English", subject1)
 }
 else {
-    alert("Invalid")
+    console.error("Marks are Between 0 to 100 for each subject");
+    
 }
 
 let subject2 = parseInt(prompt("Marks in Maths"))
@@ -42,35 +46,60 @@ console.log("Total marks", totalmarks)
 let percentage = (totalmarks / 400) * 100;
 console.log("Percentage", percentage)
 
+// let grade;
+// if (percentage >= 90) {
+//     console.log("Grade", "A+");
+//     alert(" Congratulations " + studentname + " you are pass in exams ")
+// }
+// else if (percentage >= 80 && percentage < 90) {
+//     console.log("Grade", "A");
+//     alert(" Congratulations " + studentname + " you are pass in exams ")
 
+// }
+// else if (percentage >= 70 && percentage < 80) {
+//     console.log("Grade", "B")
+//     alert(" Congratulations " + studentname + " you are pass in exams ")
+// }
+// else if (percentage >= 60 && percentage < 70) {
+//     console.log("Grade", "C")
+//     alert(" Congratulations " + studentname + " you are pass in exams ")
+
+// }
+// else if (percentage >= 50 && percentage < 60) {
+//     console.log("Grade", "D")
+//     alert(" Congratulations " + studentname + " you are pass in exams ")
+// }
+// else if (percentage < 50) {
+//     console.log("F")
+//     alert("Sorry You can't pass exams")
+// }
+
+let grade;
 if (percentage >= 90) {
-    console.log("Grade", "A+");
-    alert(" Congratulations " + studentname + " you are pass in exams ")
+    grade = "A+";
+    console.log("Grade", grade);
+    alert(`Congratulations ${studentname}, you passed the exams`);
+} else if (percentage >= 80 && percentage < 90) {
+    grade = "A";
+    console.log("Grade", grade);
+    alert(`Congratulations ${studentname}, you passed the exams`);
+} else if (percentage >= 70 && percentage < 80) {
+    grade = "B";
+    console.log("Grade", grade);
+    alert(`Congratulations ${studentname}, you passed the exams`);
+} else if (percentage >= 60 && percentage < 70) {
+    grade = "C";
+    console.log("Grade", grade);
+    alert(`Congratulations ${studentname}, you passed the exams`);
+} else if (percentage >= 50 && percentage < 60) {
+    grade = "D";
+    console.log("Grade", grade);
+    alert(`Congratulations ${studentname}, you passed the exams`);
+} else if (percentage < 50) {
+    grade = "F";
+    console.log("Grade", grade);
+    alert("Sorry, you didn't pass the exams");
 }
-else if (percentage >= 80 && percentage < 90) {
-    console.log("Grade", "A");
-    alert(" Congratulations " + studentname + " you are pass in exams ")
 
-}
-else if (percentage >= 70 && percentage < 80) {
-    console.log("Grade", "B")
-    alert(" Congratulations " + studentname + " you are pass in exams ")
-}
-else if (percentage >= 60 && percentage < 70) {
-    console.log("Grade", "C")
-    alert(" Congratulations " + studentname + " you are pass in exams ")
-
-}
-else if (percentage >= 50 && percentage < 60) {
-    console.log("Grade", "D")
-    alert(" Congratulations " + studentname + " you are pass in exams ")
-}
-else if (percentage < 50) {
-    console.log("F")
-    alert("Sorry You can't pass exams")
-}
-
-
-
-let finalresult = [" your result: ", " Your Name " , studentname , " Marks Obtained " , totalmarks , " Percentage " ,  percentage,]
-alert(finalresult)
+const finalresult = `Student Name: ${studentname}<br>Grand Total Marks: ${grandtotalmarks}<br>Total Marks: ${totalmarks}<br>Percentage: ${percentage}%<br>Grade: ${grade}`;
+document.getElementById("result").innerHTML = finalresult;
